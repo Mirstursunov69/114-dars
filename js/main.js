a9729fe10e8eb4cc2b9566f97aab542a88f85f50
Mirstursunov69 <pozilovxusan69@gmail.com>
2026-07-23 15:47:13 +0500

    tuzatdik

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let matn = document.querySelector("p").innerText.split("")
 let para = document.querySelector("p")
+para.innerText = ""
 matn.forEach((letter, ind) => {
     setTimeout(() => {
         para.innerText += letter;
